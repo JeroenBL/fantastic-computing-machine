5bb4071b33d5e24745956c836c87e062bf7cf8b4
Jeroen Blaauw <72070984+JeroenBL@users.noreply.github.com>
2024-06-02 11:37:35 +0200

    init

---
+++ README.md
@@ -0,0 +1,108 @@
+
+# {repoName}
+
+> [!IMPORTANT]
+> This repository contains the connector and configuration code only. The implementer is responsible to acquire the connection details such as username, password, certificate, etc. You might even need to sign a contract or agreement with the supplier before implementing this connector. Please contact the client's application manager to coordinate the connector requirements.
+
+<p align="center">
+  <img src="">
+</p>
+
+## Table of contents
+
+- [{repoName}](#reponame)
+  - [Table of contents](#table-of-contents)
+  - [Introduction](#introduction)
+  - [Getting started](#getting-started)
+    - [Provisioning PowerShell V2 connector](#provisioning-powershell-v2-connector)
+      - [Correlation configuration](#correlation-configuration)
+      - [Field mapping](#field-mapping)
+    - [Connection settings](#connection-settings)
+    - [Prerequisites](#prerequisites)
+    - [Remarks](#remarks)
+  - [Setup the connector](#setup-the-connector)
+  - [Getting help](#getting-help)
+  - [HelloID docs](#helloid-docs)
+
+## Introduction
+
+_{repoName}_ is a _target_ connector. _{connectorName}_ provides a set of REST API's that allow you to programmatically interact with its data. The HelloID connector uses the API endpoints listed in the table below.
+
+| Endpoint | Description |
+| -------- | ----------- |
+| /        |             |
+| /        |             |
+
+The following lifecycle actions are available:
+
+| Action                 | Description                                      |
+| ---------------------- | ------------------------------------------------ |
+| create.ps1             | PowerShell _create_ lifecycle action             |
+| delete.ps1             | PowerShell _delete_ lifecycle action             |
+| disable.ps1            | PowerShell _disable_ lifecycle action            |
+| enable.ps1             | PowerShell _enable_ lifecycle action             |
+| update.ps1             | PowerShell _update_ lifecycle action             |
+| permissions/groups/grantPermission.ps1    | PowerShell _grant_ lifecycle action              |
+| permissions/groups/revokePermission.ps1   | PowerShell _revoke_ lifecycle action             |
+| permissions/groups/permissions.ps1        | PowerShell _permissions_ lifecycle action        |
+| resources/groups/resources.ps1          | PowerShell _resources_ lifecycle action          |
+| configuration.json     | Default _configuration.json_ |
+| fieldMapping.json      | Default _fieldMapping.json_   |
+
+## Getting started
+
+### Provisioning PowerShell V2 connector
+
+#### Correlation configuration
+
+The correlation configuration is used to specify which properties will be used to match an existing account within _{connectorName}_ to a person in _HelloID_.
+
+To properly setup the correlation:
+
+1. Open the `Correlation` tab.
+
+2. Specify the following configuration:
+
+    | Setting                   | Value                             |
+    | ------------------------- | --------------------------------- |
+    | Enable correlation        | `True`                            |
+    | Person correlation field  | `PersonContext.Person.ExternalId` |
+    | Account correlation field | ``                                |
+
+> [!TIP]
+> _For more information on correlation, please refer to our correlation [documentation](https://docs.helloid.com/en/provisioning/target-systems/powershell-v2-target-systems/correlation.html) pages_.
+
+#### Field mapping
+
+The field mapping can be imported by using the _fieldMapping.json_ file.
+
+### Connection settings
+
+The following settings are required to connect to the API.
+
+| Setting  | Description                        | Mandatory |
+| -------- | ---------------------------------- | --------- |
+| UserName | The UserName to connect to the API | Yes       |
+| Password | The Password to connect to the API | Yes       |
+| BaseUrl  | The URL to the API                 | Yes       |
+
+### Prerequisites
+
+### Remarks
+
+## Setup the connector
+
+> _How to setup the connector in HelloID._ Are special settings required. Like the _primary manager_ settings for a source connector.
+
+## Getting help
+
+> [!TIP]
+> _For more information on how to configure a HelloID PowerShell connector, please refer to our [documentation](https://docs.helloid.com/en/provisioning/target-systems/powershell-v2-target-systems.html) pages_.
+
+> [!TIP]
+>  _If you need help, feel free to ask questions on our [forum](https://forum.helloid.com)_.
+
+## HelloID docs
+
+The official HelloID documentation can be found at: https://docs.helloid.com/
+
---
+++ create.ps1
@@ -0,0 +1,169 @@
+#################################################
+# {repoName}-Create
+# PowerShell V2
+#################################################
+
+# Enable TLS1.2
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
+
+#region functions
+function Invoke-{connectorName}RestMethod {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Method,
+
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Uri,
+
+        [object]
+        $Body,
+
+        [string]
+        $ContentType = 'application/json',
+
+        [Parameter(Mandatory)]
+        [System.Collections.IDictionary]
+        $Headers
+    )
+
+    process {
+        try {
+            $splatParams = @{
+                Uri         = $Uri
+                Headers     = $Headers
+                Method      = $Method
+                ContentType = $ContentType
+            }
+
+            if ($Body) {
+                $splatParams['Body'] = $Body
+            }
+            Invoke-RestMethod @splatParams -Verbose:$false
+        } catch {
+            $PSCmdlet.ThrowTerminatingError($_)
+        }
+    }
+}
+
+function Resolve-{connectorName}Error {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [object]
+        $ErrorObject
+    )
+    process {
+        $httpErrorObj = [PSCustomObject]@{
+            ScriptLineNumber = $ErrorObject.InvocationInfo.ScriptLineNumber
+            Line             = $ErrorObject.InvocationInfo.Line
+            ErrorDetails     = $ErrorObject.Exception.Message
+            FriendlyMessage  = $ErrorObject.Exception.Message
+        }
+        if (-not [string]::IsNullOrEmpty($ErrorObject.ErrorDetails.Message)) {
+            $httpErrorObj.ErrorDetails = $ErrorObject.ErrorDetails.Message
+        } elseif ($ErrorObject.Exception.GetType().FullName -eq 'System.Net.WebException') {
+            if ($null -ne $ErrorObject.Exception.Response) {
+                $streamReaderResponse = [System.IO.StreamReader]::new($ErrorObject.Exception.Response.GetResponseStream()).ReadToEnd()
+                if (-not [string]::IsNullOrEmpty($streamReaderResponse)) {
+                    $httpErrorObj.ErrorDetails = $streamReaderResponse
+                }
+            }
+        }
+        try {
+            $errorDetailsObject = ($httpErrorObj.ErrorDetails | ConvertFrom-Json)
+            # Make sure to inspect the error result object and add only the error message as a FriendlyMessage.
+            # $httpErrorObj.FriendlyMessage = $errorDetailsObject.message
+            $httpErrorObj.FriendlyMessage = $httpErrorObj.ErrorDetails # Temporarily assignment
+        } catch {
+            $httpErrorObj.FriendlyMessage = $httpErrorObj.ErrorDetails
+        }
+        Write-Output $httpErrorObj
+    }
+}
+#endregion
+
+try {
+    # Initial Assignments
+    $outputContext.AccountReference = 'Currently not available'
+
+    # Validate correlation configuration
+    if ($actionContext.CorrelationConfiguration.Enabled) {
+        $correlationField = $actionContext.CorrelationConfiguration.accountField
+        $correlationValue = $actionContext.CorrelationConfiguration.accountFieldValue
+
+        if ([string]::IsNullOrEmpty($($correlationField))) {
+            throw 'Correlation is enabled but not configured correctly'
+        }
+        if ([string]::IsNullOrEmpty($($correlationValue))) {
+            throw 'Correlation is enabled but [accountFieldValue] is empty. Please make sure it is correctly mapped'
+        }
+
+        # Verify if a user must be either [created ] or just [correlated]
+        $correlatedAccount = 'userInfo'
+    }
+
+    if ($null -ne $correlatedAccount) {
+        $action = 'CorrelateAccount'
+    } else {
+        $action = 'CreateAccount'
+    }
+
+    # Add a message and the result of each of the validations showing what will happen during enforcement
+    if ($actionContext.DryRun -eq $true) {
+        Write-Information "[DryRun] $action {connectorName} account for: [$($personContext.Person.DisplayName)], will be executed during enforcement"
+    }
+
+    # Process
+    if (-not($actionContext.DryRun -eq $true)) {
+        switch ($action) {
+            'CreateAccount' {
+                Write-Information 'Creating and correlating {connectorName} account'
+
+                # Make sure to test with special characters and if needed; add utf8 encoding.
+
+                $outputContext.Data = $createdAccount
+                $outputContext.AccountReference = ''
+                $auditLogMessage = "Create account was successful. AccountReference is: [$($outputContext.AccountReference)"
+                break
+            }
+
+            'CorrelateAccount' {
+                Write-Information 'Correlating {connectorName} account'
+
+                $outputContext.Data = $correlatedAccount
+                $outputContext.AccountReference = ''
+                $outputContext.AccountCorrelated = $true
+                $auditLogMessage = "Correlated account: [$($correlatedAccount.ExternalId)] on field: [$($correlationField)] with value: [$($correlationValue)]"
+                break
+            }
+        }
+
+        $outputContext.success = $true
+        $outputContext.AuditLogs.Add([PSCustomObject]@{
+                Action  = $action
+                Message = $auditLogMessage
+                IsError = $false
+            })
+    }
+} catch {
+    $outputContext.success = $false
+    $ex = $PSItem
+    if ($($ex.Exception.GetType().FullName -eq 'Microsoft.PowerShell.Commands.HttpResponseException') -or
+        $($ex.Exception.GetType().FullName -eq 'System.Net.WebException')) {
+        $errorObj = Resolve-{connectorName}Error -ErrorObject $ex
+        $auditMessage = "Could not create or correlate {connectorName} account. Error: $($errorObj.FriendlyMessage)"
+        Write-Warning "Error at Line '$($errorObj.ScriptLineNumber)': $($errorObj.Line). Error: $($errorObj.ErrorDetails)"
+    } else {
+        $auditMessage = "Could not create or correlate {connectorName} account. Error: $($ex.Exception.Message)"
+        Write-Warning "Error at Line '$($ex.InvocationInfo.ScriptLineNumber)': $($ex.InvocationInfo.Line). Error: $($ex.Exception.Message)"
+    }
+    $outputContext.AuditLogs.Add([PSCustomObject]@{
+            Message = $auditMessage
+            IsError = $true
+        })
+}
